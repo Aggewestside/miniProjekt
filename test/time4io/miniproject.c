7011a0c8b79d75e3ae36c4d97f505aa49f573c1f
Keykal <43344383+Keykal@users.noreply.github.com>
2019-02-27 13:18:00 +0100

    pprogresss

--- test/time4io/miniproject.c
+++ test/time4io/miniproject.c
@@ -15,21 +15,34 @@
 #include "mipslab.h"  /* Declarations for these labs */
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 //int mytime = 0x5957;
 int tickcount = 128;
 volatile int * trise;
 volatile int * porte;
+int resetcount = 0;
 int i = 0;
-//int timerresetcounter = 0;
+int DELAY = 1000;
+int n = 10;
 
-char textstring[] = "text, more text, and even more text!";
 
 /* Interrupt Service Routine */
-void user_isr( void )
+void user_isr( void ) 
 {
-  return;
-}
+	
+	
+	if(IFS(0) & 0x100)			/* Test time-out event flag */ 
+	{
+		resetcount++;
+		if(resetcount == 10)
+		{
+			resetcount = 0;
+		}
+		IFSCLR(0) = 0x100;				/* Reset event flag */
+	}
+	return;
+} 
 
 /* Lab-specific initialization goes here */
 void labinit( void )
@@ -37,43 +50,82 @@ void labinit( void )
 	volatile int * trise = (volatile int *) 0xbf886100;
 	*trise = *trise & 0xffffff00;
 	TRISD = 0xfe0 | TRISD;
- 
+	
+	
+	T2CON = 0x70; 
+	PR2 = (80000000 / 256) / 10;
+	TMR2 = 0;
+	T2CONSET = 0x8000; /* Start the timer */
+	
+	IECSET(0) = 0x00000100;
+	/* IPCSET(0) = 0x0000001E; */
+	IPCSET(2) = 0x0000001F;
+	enable_interrupt();
   return;
 }
  
 /* This function is called repetitively from the main program */
 void labwork( void )
 {
-// time2string( textstring, mytime );
-  //display_string( 3, textstring );
-  //display_update();
-  //tick( &mytime );
+	while (tickcount == 128)
+	{
+		PORTE = tickcount;
+		
+		if(getbtns() == 4)
+		{
+			n-=1;
+			break;
+		}
+	}
+		
+		
+		
+		
 		if (tickcount == 128)
 		{
 			PORTE = tickcount;
 			//delay (1000);
 			
-			for(i = 0; i < 7; i++)
+			for(i = 0; i < 6; i++)
 			{
 				tickcount = tickcount >> 1;
 				PORTE = tickcount;
-				delay (1000);
+				delay (3);
 				//timerresetcounter++;
 			}
+			tickcount = tickcount >> 1;
+				PORTE = tickcount;
+				
 		}
+	
+		
+	while (tickcount == 1)
+	{
+		PORTE = tickcount;
+		
+		if(getbtns() == 1)
+		{
+			n-=1;
+			break;
+		}
+	}
+	
 	
 		if(tickcount == 1);
 		{
 			PORTE = tickcount;
 			//delay (1000);
 			
-			for(i = 0; i < 7; i++)
+			for(i = 0; i < 6; i++)
 			{
 				tickcount = tickcount << 1;
 				PORTE = tickcount;
-				delay (1000);
+				delay (3);
 				//timerresetcounter++;
 			}
+			tickcount = tickcount << 1;
+				PORTE = tickcount;
+				
 		}
 
   
@@ -87,5 +139,5 @@ void labwork( void )
 	  mytime = ((mytime & 0xff0f) | (getsw() << 4)); 
   
   */
-  display_image(96, icon);
+
 }
